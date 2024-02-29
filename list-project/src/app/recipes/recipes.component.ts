139628c01d632b75674fe7d615e33dabf3ea3152
amadejsky <adrian.madejski@vp.pl>
2024-02-29 19:22:11 +0100

    another exercise, shopping list with prodict details project

---
+++ list-project/src/app/recipes/recipes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { defaultUrlMatcher } from '@angular/router';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrl: './recipes.component.css'
+})
+export class RecipesComponent {
+
+}
