c641c945520fa3fd0d28cb855b59a338a7a70703
amadejsky <adrian.madejski@vp.pl>
2024-04-15 18:53:20 +0200

    static forms approach exercise, in forms-td-start, reactive forms approach

---
+++ forms-reactive-start/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+         'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required,Validators.email]),
+      }),
+     
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([]) 
+    });
+  }
+
+  onSubmit(){
+    console.log(this.signupForm);
+
+    // this.signupForm.reset();
+  }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+}
--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -7,9 +7,37 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupForm: NgForm;
+  @ViewChild('form') signupForm: NgForm;
+  mail: string;
+  sub: string;
+  pswd: string;
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  // defaultQuestion = 'pet';
+  // answer='';
+  // genders = ['male', 'female'];
+  // user = {
+  //   username: '',
+  //   email: '',
+  //   secretQ: '',
+  //   answer: '',
+  //   gender: ''
+  // }
+  submittedForm: boolean = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+    }});
   }
 
   // onSubmit(form: NgForm){
@@ -17,6 +45,10 @@ export class AppComponent {
   // }
 
   onSubmit(){
-    console.log(this.signupForm);
+    console.log(this.signupForm.value);
+    this.mail=this.signupForm.value.mail;
+    this.sub=this.signupForm.value.sub.value;
+    this.pswd=this.signupForm.value.password;
+    this.signupForm.reset();
   }
 }
