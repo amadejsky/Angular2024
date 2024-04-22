c676ab48ba55a509ca3b20bffba5ed75bd91508c
amadejsky <adrian.madejski@vp.pl>
2024-04-22 17:00:02 +0200

    Backen, Firebase project connection get and fetch data

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,18 +11,44 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
+
+    this.http.post(
+      'https://angular-ms-project-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+       postData
+      ).subscribe(responseData => {
+        console.log(responseData);
+      });
+
   }
 
   onFetchPosts() {
-    // Send Http request
+   this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http.get(
+      'https://angular-ms-project-default-rtdb.europe-west1.firebasedatabase.app/posts.json'
+    )
+    .pipe(map(responseData => {
+      const postsArray = [];
+      for (const key in responseData){
+        if(responseData.hasOwnProperty(key)){
+          postsArray.push({ ...responseData[key], id: key })
+        }
+      }
+      return postsArray;
+    })
+  ).subscribe(data => {
+      console.log(data);
+    });
   }
 }
