db2bd3cc2a28e525555f58a6c91267b94f857817
amadejsky <adrian.madejski@vp.pl>
2024-05-14 10:33:45 +0200

    dynamically rendered components

--- list-project/src/app/app.module.ts
+++ list-project/src/app/app.module.ts
@@ -21,6 +21,8 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component' ;
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,7 +38,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -47,5 +51,6 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
   ],
   providers: [RecipeService, ShoppingListService, AppComponent, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService,multi: true}],
   bootstrap: [AppComponent]
+ 
 })
 export class AppModule { }
